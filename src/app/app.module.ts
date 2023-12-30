f488d85617d591797b4890289991580ccfde59be
Morelle-Olivia <hotoumorelle@gmail.com>
2023-12-30 13:59:51 +0100

    Created a view all transaction page and designed it.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LandingPageComponent } from './pages/landing-page/landing-page.component';
 import { TransactionsComponent } from './pages/transactions/transactions.component';
+import { ViewAllTransactionsComponent } from './pages/view-all-transactions/view-all-transactions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     LandingPageComponent,
-    TransactionsComponent
+    TransactionsComponent,
+    ViewAllTransactionsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pages/view-all-transactions/view-all-transactions.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-all-transactions',
+  templateUrl: './view-all-transactions.component.html',
+  styleUrls: ['./view-all-transactions.component.css']
+})
+export class ViewAllTransactionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
